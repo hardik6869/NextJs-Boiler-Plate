379c016d35648d6156a251f13af2b69fbe3727f1
hardik <hardik@gmail.com>
2022-05-25 17:56:36 +0530

    Added: babel & tailwind css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,20 @@
-const HomePage = () => {
-    return <div> Welcome to Next.js!</div>;
+const index = () => {
+    return (
+        <>
+            <div>
+                <p className="text-gray-100 p-2 bg-slate-800 shadow-1xl shadow-gray-600  text-center text-2xl font-bold ">
+                    Welcome to Next.js!
+                </p>
+                <img
+                    src="https://ui-lib.com/blog/wp-content/uploads/2021/12/Cover-next-js-boilerplates.jpg"
+                    className="h-40 w-50"
+                />
+                <p className="text-blue-400 text-2xl  font-extrabold text-center">
+                    Typescript
+                </p>
+            </div>
+        </>
+    );
 };
 
-export default HomePage;
+export default index;
