951463fc717f7edceab17b4af8dc1e5591b228d9
hardik <hardik@gmail.com>
2022-05-25 10:59:05 +0530

    Added: babel, tailwindcss & add dependencies

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+const index = () => {
+    return (
+        <>
+            <div>
+                <p className="color-blue-500 font-extrabold text-center">
+                    Welcome to Next.js!
+                </p>
+                <img
+                    src="https://ui-lib.com/blog/wp-content/uploads/2021/12/Cover-next-js-boilerplates.jpg"
+                    className="h-40 w-50"
+                />
+                <p className="color-blue-500 font-extrabold text-center">
+                    Javascript
+                </p>
+            </div>
+        </>
+    );
+};
+
+export default index;
