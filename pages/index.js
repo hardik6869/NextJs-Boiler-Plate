da0c24e2542b3903c42c74a0249756f0dd953447
hardik <hardik@gmail.com>
2022-05-25 18:15:35 +0530

    eslint

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,14 @@ const index = () => {
     return (
         <>
             <div>
-                <p className="color-blue-500 font-extrabold text-center">
+                <p className="text-gray-100 p-2 bg-slate-800 shadow-1xl shadow-gray-600  text-center text-2xl font-bold ">
                     Welcome to Next.js!
                 </p>
                 <img
                     src="https://ui-lib.com/blog/wp-content/uploads/2021/12/Cover-next-js-boilerplates.jpg"
                     className="h-40 w-50"
                 />
-                <p className="color-blue-500 font-extrabold text-center">
+                <p className="text-blue-400 text-2xl  font-extrabold text-center">
                     Javascript
                 </p>
             </div>
