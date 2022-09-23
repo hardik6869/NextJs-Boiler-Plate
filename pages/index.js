4651e69013aaeaffeb66436a9cf3ea71e34deea2
hardik6869 <sorthiyahardik6869@gmail.com>
2022-09-23 09:28:54 +0530

    Polishing

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,18 @@
-const index = () => {
+const HomePage = () => {
     return (
-        <>
-            <div>
-                <p className="text-gray-100 p-2 bg-slate-800 shadow-1xl shadow-gray-600  text-center text-2xl font-bold ">
-                    Welcome to Next.js!
-                </p>
-                <img
-                    src="https://ui-lib.com/blog/wp-content/uploads/2021/12/Cover-next-js-boilerplates.jpg"
-                    className="h-40 w-50"
-                />
-                <p className="text-blue-400 text-2xl  font-extrabold text-center">
-                    Javascript
-                </p>
-            </div>
-        </>
+        <div>
+            <p className="text-gray-100 p-2 bg-slate-800 shadow-1xl shadow-gray-600  text-center text-2xl font-bold ">
+                Welcome to Next.js!
+            </p>
+            <img
+                src="https://ui-lib.com/blog/wp-content/uploads/2021/12/Cover-next-js-boilerplates.jpg"
+                className="h-40 w-50"
+            />
+            <p className="text-blue-400 text-2xl  font-extrabold text-center">
+                Javascript
+            </p>
+        </div>
     );
 };
 
-export default index;
+export default HomePage;
